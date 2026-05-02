9b958662df7fedce8b1575807635f33712183028
Oscar Perdomo <osdo-persa.x@hotmail.com>
2026-05-01 22:46:00 -0500

    fix:STORAGES

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -161,6 +161,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
